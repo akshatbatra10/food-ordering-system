d9fc4fdbfc062bcb07672d9ad1c79c47b8678f18
akshatbatra10 <62565788+akshatbatra10@users.noreply.github.com>
2021-08-24 20:09:40 +0530

    displayed all restaurants in database

--- index.js
+++ index.js
@@ -2,9 +2,28 @@ if (process.env.NODE_ENV !== "production") {
   require("dotenv").config();
 }
 const express = require("express");
+const mongoose = require("mongoose");
 const path = require("path");
 
+const Restaurants = require("./models/restaurants");
+
 const app = express();
+
+mongoose.connect(
+  "mongodb+srv://AkshatBatra:g0t0@he11@cluster0.gia1c.mongodb.net/restaurantData?retryWrites=true&w=majority",
+  {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+    connectTimeoutMS: 3000,
+  }
+);
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", function () {
+  console.log("Database connected");
+});
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
@@ -12,6 +31,15 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", (req, res) => {
   res.render("home");
+});
+
+app.get("/restaurants", async (req, res) => {
+  try {
+    const restaurants = await Restaurants.find({});
+    res.render("restaurants/index", { restaurants });
+  } catch (e) {
+    console.log(e);
+  }
 });
 
 const PORT = process.env.PORT || 3000;
--- seed/index.js
+++ seed/index.js
@@ -1,25 +1,26 @@
-const mongoose = require('mongoose');
-const Restaurants = require('../models/restaurants');
-const restaurantData = require('./restaurantData');
+const mongoose = require("mongoose");
+const Restaurants = require("../models/restaurants");
+const restaurantData = require("./restaurantData");
 
-mongoose.connect('mongodb+srv://AkshatBatra:g0t0@he11@cluster0.gia1c.mongodb.net/restaurantData?retryWrites=true&w=majority', {
-  useNewUrlParser: true,
-  useCreateIndex: true,
-  useUnifiedTopology: true,
-  connectTimeoutMS: 3000
-});
+mongoose.connect(
+  "mongodb+srv://AkshatBatra:g0t0@he11@cluster0.gia1c.mongodb.net/restaurantData?retryWrites=true&w=majority",
+  {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+    connectTimeoutMS: 3000,
+  }
+);
 
 const db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function() {
-  console.log('Database connected');
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", function () {
+  console.log("Database connected");
 });
-
-restaurantData[0].restaurants.map(data => console.log(data.restaurant.name));
 
 const seedDB = async () => {
   await Restaurants.deleteMany({});
-  try{
+  try {
     restaurantData[0].restaurants.map(async (data) => {
       const rest = new Restaurants({
         name: data.restaurant.name,
@@ -27,20 +28,22 @@ const seedDB = async () => {
         image: data.restaurant.featured_image,
         cuisines: data.restaurant.cuisines.split(", "),
         geometry: {
-          type: 'Point',
-          coordinates: [data.restaurant.location.longitude, data.restaurant.location.latitude]
+          type: "Point",
+          coordinates: [
+            data.restaurant.location.longitude,
+            data.restaurant.location.latitude,
+          ],
         },
         address: data.restaurant.location.address,
         city: data.restaurant.location.city,
-        average_cost_for_two: data.restaurant.average_cost_for_two
-      })
+        average_cost_for_two: data.restaurant.average_cost_for_two,
+      });
       await rest.save();
-    })
-  }
-  catch(e) {
+    });
+  } catch (e) {
     console.log(e.error);
   }
-}
+};
 
 seedDB();
 // seedDB().then(() => mongoose.connection.close());
