67c1e19c8f37e57d10ad11dce78b7316508d8286
akshatbatra10 <62565788+akshatbatra10@users.noreply.github.com>
2021-08-24 18:27:42 +0530

    added location autocomplete using locationiq

--- index.js
+++ index.js
@@ -1,17 +1,23 @@
-if(process.env.NODE_ENV !== 'production'){
-  require('dotenv').config();
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
 }
 const express = require("express");
 const path = require("path");
 
 const app = express();
 
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
-app.use(express.static(path.join(__dirname, 'public')));
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", (req, res) => {
-  res.render('home.ejs');
+  res.render("home.ejs");
+});
+app.get("/search", (req, res) => {
+  res.render("searchbox.ejs");
+});
+app.get("/searchb", (req, res) => {
+  res.render("search.ejs");
 });
 
 const PORT = process.env.PORT || 3000;
