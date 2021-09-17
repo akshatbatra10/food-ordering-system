a2f6d760f3070d8ff6459e2b1565115035dff38a
akshatbatra10 <62565788+akshatbatra10@users.noreply.github.com>
2021-09-17 16:00:32 +0530

    added front-end folder and made locate me feature to display address in search box

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ if (process.env.NODE_ENV !== "production") {
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
+const helmet = require("helmet");
 const path = require("path");
 
 const routes = require("./routes/routes");
@@ -29,6 +30,7 @@ app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
 app.use(cors());
+app.use(helmet());
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "public")));
 
