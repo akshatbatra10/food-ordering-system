67e5137fe38f2bd425546f1ad845b5d83bac01b1
akshatbatra10 <62565788+akshatbatra10@users.noreply.github.com>
2021-09-21 22:13:34 +0530

    completed login and register page and refactored code

--- frontend/javascript/signin-signup.js
+++ frontend/javascript/signin-signup.js
@@ -1,11 +1,11 @@
-const signInButton = document.querySelector("#sign-in-button");
-const signUpButton = document.querySelector("#sign-up-button");
-const container = document.querySelector(".container");
+const sign_in_btn = document.querySelector('#sign-in-btn');
+const sign_up_btn = document.querySelector('#sign-up-btn');
+const container = document.querySelector('.container');
 
-signUpButton.addEventListener('click', () => {
-    console.log('hi')
-    container.classList.add('sign-up-mode');
+sign_up_btn.addEventListener('click', () => {
+  container.classList.add('sign-up-mode');
 });
-signInButton.addEventListener('click', () => {
-    container.classList.remove('sign-up-mode');
+
+sign_in_btn.addEventListener('click', () => {
+  container.classList.remove('sign-up-mode');
 });
