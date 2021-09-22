098f702fe45baff39615012d9f6f53a4bf0cfb72
akshatbatra10 <62565788+akshatbatra10@users.noreply.github.com>
2021-09-22 22:51:58 +0530

    added new data for restaurants

--- seed/index.js
+++ seed/index.js
@@ -1,6 +1,12 @@
 const mongoose = require("mongoose");
-const Restaurants = require("../models/restaurants");
-const restaurantData = require("./restaurantData");
+const Restaurants = require("./models/restaurants");
+// const restaurantData = require("./restaurantData");
+const file1 = require("./file1");
+const file2 = require("./file2");
+const file3 = require("./file3");
+const file4 = require("./file4");
+
+// const citiesArray = ['New Delhi', 'Mumbai', 'Pune', 'Agra', 'Banglore', 'Ahmedabad', 'Chennai', 'Surat', 'Hyderabad', 'Kolkata', 'Jaipur']
 
 mongoose.connect(
   "mongodb+srv://AkshatBatra:g0t0@he11@cluster0.gia1c.mongodb.net/restaurantData?retryWrites=true&w=majority",
@@ -21,25 +27,90 @@ db.once("open", function () {
 const seedDB = async () => {
   await Restaurants.deleteMany({});
   try {
-    restaurantData.map(restaurant => restaurant.restaurants.map(async (data) => {
-      const rest = new Restaurants({
-        name: data.restaurant.name,
-        has_online_delivery: data.restaurant.has_online_delivery,
-        image: data.restaurant.featured_image,
-        cuisines: data.restaurant.cuisines.split(", "),
-        geometry: {
-          type: "Point",
-          coordinates: [
-            data.restaurant.location.longitude,
-            data.restaurant.location.latitude,
-          ],
-        },
-        address: data.restaurant.location.address,
-        city: data.restaurant.location.city,
-        average_cost_for_two: data.restaurant.average_cost_for_two,
-      });
-      await rest.save();
-    }));
+    file1.map((restaurant) =>
+      restaurant.restaurants.map(async (data) => {
+        const rest = new Restaurants({
+          name: data.restaurant.name,
+          has_online_delivery: data.restaurant.has_online_delivery,
+          image: data.restaurant.featured_image,
+          cuisines: data.restaurant.cuisines.split(", "),
+          geometry: {
+            type: "Point",
+            coordinates: [
+              data.restaurant.location.longitude,
+              data.restaurant.location.latitude,
+            ],
+          },
+          address: data.restaurant.location.address,
+          city: data.restaurant.location.city,
+          average_cost_for_two: data.restaurant.average_cost_for_two,
+        });
+        await rest.save();
+      })
+    );
+    file2.map((restaurant) =>
+      restaurant.restaurants.map(async (data) => {
+        const rest = new Restaurants({
+          name: data.restaurant.name,
+          has_online_delivery: data.restaurant.has_online_delivery,
+          image: data.restaurant.featured_image,
+          cuisines: data.restaurant.cuisines.split(", "),
+          geometry: {
+            type: "Point",
+            coordinates: [
+              data.restaurant.location.longitude,
+              data.restaurant.location.latitude,
+            ],
+          },
+          address: data.restaurant.location.address,
+          city: data.restaurant.location.city,
+          average_cost_for_two: data.restaurant.average_cost_for_two,
+        });
+        await rest.save();
+      })
+    );
+    file3.map((restaurant) =>
+      restaurant.restaurants.map(async (data) => {
+        const rest = new Restaurants({
+          name: data.restaurant.name,
+          has_online_delivery: data.restaurant.has_online_delivery,
+          image: data.restaurant.featured_image,
+          cuisines: data.restaurant.cuisines.split(", "),
+          geometry: {
+            type: "Point",
+            coordinates: [
+              data.restaurant.location.longitude,
+              data.restaurant.location.latitude,
+            ],
+          },
+          address: data.restaurant.location.address,
+          city: data.restaurant.location.city,
+          average_cost_for_two: data.restaurant.average_cost_for_two,
+        });
+        await rest.save();
+      })
+    );
+    file4.map((restaurant) =>
+      restaurant.restaurants.map(async (data) => {
+        const rest = new Restaurants({
+          name: data.restaurant.name,
+          has_online_delivery: data.restaurant.has_online_delivery,
+          image: data.restaurant.featured_image,
+          cuisines: data.restaurant.cuisines.split(", "),
+          geometry: {
+            type: "Point",
+            coordinates: [
+              data.restaurant.location.longitude,
+              data.restaurant.location.latitude,
+            ],
+          },
+          address: data.restaurant.location.address,
+          city: data.restaurant.location.city,
+          average_cost_for_two: data.restaurant.average_cost_for_two,
+        });
+        await rest.save();
+      })
+    );
   } catch (e) {
     console.log(e.error);
   }
---
+++ seed/models/restaurants.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const restaurantSchema = new Schema({
+  name: String,
+  has_online_delivery: Number,
+  image: String,
+  cuisines: [String],
+  geometry: {
+    type: {
+      type: String,
+      enum: ["Point"],
+      require: true,
+    },
+    coordinates: {
+      type: [Number],
+      require: true,
+    },
+  },
+  address: String,
+  city: String,
+  average_cost_for_two: Number,
+});
+
+module.exports = mongoose.model("Restaurants", restaurantSchema);
