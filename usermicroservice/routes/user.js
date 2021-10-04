560e2d605b08ff3e5e9079eaa640ab1621af6245
akshatbatra10 <62565788+akshatbatra10@users.noreply.github.com>
2021-10-04 15:11:58 +0530

    implemented jwt refresh token

--- usermicroservice/routes/user.js
+++ usermicroservice/routes/user.js
@@ -6,6 +6,25 @@ const Users = require("../models/users");
 
 const router = express.Router();
 
+let refreshTokens = [];
+
+function generateAccessToken(user) {
+  return jwt.sign({ _id: user._id }, process.env.ACCESS_TOKEN, {
+    expiresIn: "15s",
+  });
+}
+
+router.post("/token", (req, res) => {
+  const refreshToken = req.body.token;
+  if (refreshToken === null) return res.sendStatus(401);
+  if (!refreshTokens.includes(refreshToken)) return res.sendStatus(403);
+  jwt.verify(refreshToken, process.env.REFRESH_ACCESS_TOKEN, (err, user) => {
+    if (err) return res.sendStatus(403);
+    const accessToken = generateAccessToken(user);
+    res.json({ accessToken: accessToken });
+  });
+});
+
 router.post("/registeruser", async (req, res) => {
   try {
     const salt = await bcrypt.genSalt();
@@ -29,8 +48,18 @@ router.post("/login", async (req, res) => {
   const validPass = await bcrypt.compare(req.body.password, user.password);
   if (!validPass) return res.status(400).send("Incorrect password");
 
-  const token = jwt.sign({ _id: user._id }, process.env.ACCESS_TOKEN);
-  res.json({ token: token });
+  const token = generateAccessToken(user);
+  const refreshToken = jwt.sign(
+    { _id: user._id },
+    process.env.REFRESH_ACCESS_TOKEN
+  );
+  refreshTokens.push(refreshToken);
+  res.status(200).json({ token: token, refreshToken: refreshToken });
+});
+
+router.delete("/logout", (req, res) => {
+  refreshTokens = refreshTokens.filter((token) => token !== req.body.token);
+  res.sendStatus(204);
 });
 
 module.exports = router;
