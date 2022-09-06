ac42b82591eb6e1b13554bccb4b5a508e129f716
akshatbatra10 <62565788+akshatbatra10@users.noreply.github.com>
2022-09-06 21:35:23 +0530

    displayed restaurant details on the checkout page

--- frontend/javascript/checkout_page.js
+++ frontend/javascript/checkout_page.js
@@ -1,2 +1,29 @@
 const id = window.localStorage.getItem("restaurantID");
-console.log(id);
+const restaurantImage = document.querySelector(".restaurant-image");
+const restaurantName = document.querySelector(".restaurant-name");
+const restaurantAdd = document.querySelector(".restaurant-address");
+
+let restaurant;
+
+const fetchData = async () => {
+  try {
+    const response = await fetch(
+      `http://localhost:3000/restaurants/${id}?${Date.now()}`
+    );
+    restaurant = await response.json();
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+function addRestaurant() {
+  console.log(restaurant);
+  restaurantImage.src = restaurant.image;
+  restaurantName.innerText = restaurant.name;
+  restaurantAdd.innerText = restaurant.address;
+}
+
+window.addEventListener("load", async function () {
+  await fetchData();
+  addRestaurant();
+});
